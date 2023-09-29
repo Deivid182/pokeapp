2c068cda673a63e47436a24cb5e2fb107688e8f5
Deivid182 <davidmendoza911119@gmail.com>
2023-09-28 23:10:26 -0600

    fixed the not found evolutions

--- src/pages/pokemon-page.tsx
+++ src/pages/pokemon-page.tsx
@@ -43,7 +43,7 @@ const PokemonPage = () => {
 
         pokeArray.push({ name: pokemonLv1, img: pokemonLv1Img });
 
-        if(data.chain.evolves_to.length !== 0) {
+        if (data.chain.evolves_to.length !== 0) {
           const pokemonLv2 = data.chain.evolves_to[0].species.name;
           const pokemonLv2Img = await getPokemonImgs(pokemonLv2);
 
@@ -59,7 +59,7 @@ const PokemonPage = () => {
             setPokeEvoArray(pokeArray);
           }
         }
-        
+
       } catch (error) {
         if (error instanceof AxiosError) {
           console.log(error.response?.data);
@@ -77,7 +77,7 @@ const PokemonPage = () => {
   ) : (
     <div className='flex flex-col gap-4'>
       <h3 className='py-4 relative text-center text-2xl font-semibold text-gray-900 uppercase'>
-        <hr className='absolute w-full border border-neutral-500'/>
+        <hr className='absolute w-full border border-neutral-500' />
         {pokeEvoArray.length > 0 && <span className='absolute top-1/2 -translate-y-1/2 left-1/2 -translate-x-1/2 bg-gray-100 px-2 py-1'>{pokeEvoArray[0].name}</span>}
       </h3>
       <div className='relative flex-1 w-full max-w-sm mx-auto bg-sky-600 rounded-lg flex items-center justify-center'>
@@ -87,26 +87,34 @@ const PokemonPage = () => {
           className='object-contain'
         />
       </div>
-      <p className='text-center text-xl font-semibold text-gray-700 uppercase'>
-        Evolutions
-      </p>
-      <div className='flex gap-3'>
-        {pokeEvoArray.map((evo, index) => (
-          <div
-            className='flex-1 relative w-full p-2 bg-gray-100 rounded-lg'
-            key={index}
-          >
-            <div className='bg-sky-200 rounded-lg'>
-              <img
-                src={evo.img}
-                alt={evo.name}
-                className='object-contain w-full h-full rounded-lg'
-              />
-            </div>
-            <p className='text-center pt-2 text-xl uppercase font-semibold text-neutral-700'>{evo.name}</p>
+      {pokeEvoArray.length > 0 ? (
+        <>
+          <p className='text-center text-xl font-semibold text-gray-700 uppercase'>
+            Evolutions
+          </p>
+          <div className='flex gap-3'>
+            {pokeEvoArray.map((evo, index) => (
+              <div
+                className='flex-1 relative w-full p-2 bg-gray-100 rounded-lg'
+                key={index}
+              >
+                <div className='bg-sky-200 rounded-lg'>
+                  <img
+                    src={evo.img}
+                    alt={evo.name}
+                    className='object-contain w-full h-full rounded-lg'
+                  />
+                </div>
+                <p className='text-center pt-2 text-xl uppercase font-semibold text-neutral-700'>{evo.name}</p>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      ) : (
+        <p className='text-center text-xl font-semibold text-gray-700 uppercase'>
+          There's no evolutions for this pokemon
+        </p>
+      )}
     </div>
   );
 };
